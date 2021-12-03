65e2cd7ea2888c1974a0d8d8659889955fc56a4b
Zzzup <77539283+Zzzupp@users.noreply.github.com>
2021-12-03 22:20:23 +0200

    Update script.js

--- 8-burgersecound/script.js
+++ 8-burgersecound/script.js
@@ -10,22 +10,24 @@ open.addEventListener('click', event => {
     menu.classList.add('open');
     open.style.opacity = '0';
     close.style.opacity = '1';
-    close.style.visibility= 'hidden';
+    close.style.visibility= 'visible';
 })
 
 close.addEventListener('click', event => {
     menu.classList.remove('open');
     open.style.opacity = '1';
     close.style.opacity = '0';
-    close.style.visibility= 'visible';
+    close.style.visibility= 'hidden';
 })
-// window.addEventListener('click', (event) =>{
-//     if(event.target !== hidemenu && event.target !== open){
-//         hidemenu.classList.remove('open');
-//     }
-//     open.style.opacity = '1';
-//     close.style.opacity = '0';
-// })
+ window.addEventListener('click', (event) =>{
+        if(!menu.contains(event.target) && !open.contains(event.target)){
+            menu.classList.remove('open');
+            open.style.opacity = '1';
+            close.style.opacity = '0';
+            close.style.visibility= 'hidden';
+    
+        }
+    })
 
 
  
